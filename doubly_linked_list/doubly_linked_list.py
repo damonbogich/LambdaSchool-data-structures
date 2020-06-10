765a9d96dd95b2a4172090919e967c1b2048de91
damonbogich <damonbogich@gmail.com>
2020-06-10 09:27:56 -0400

    got delete and max working

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -167,24 +167,67 @@ class DoublyLinkedList:
             self.length -= 1
 
             
-
-
-
-            
-
-
-
-
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        #need to make sure it is not empty and there isn't just one
+        if not self.head and self.tail:
+            return None
+        elif self.head == self.tail:
+            return None
+        elif self.length == 2:
+            current_head = self.head
+            current_tail = self.tail
+
+            self.head = current_tail
+            self.tail = current_head
+            self.head.prev = None
+            self.head.next = self.tail
+            self.tail.prev = self.head
+            self.tail.next = None
+        else:
+            self.delete(node)
+            self.add_to_tail(node.value)
+            self.length -= 1
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+        if not self.head and self.tail:
+            return None
+        elif self.head == self.tail:
+            self.head = None
+            self.tail = None
+            self.length -= 1
+        elif self.head is node:
+            current_head = self.head
+            self.head = current_head.next
+            self.head.prev = None
+            self.length -=1
+        elif self.tail is node:
+            current_tail = self.tail
+            self.tail = current_tail.prev
+            self.tail.next = None
+            self.length =-1
+        else:
+            self.delete(node)
+            #why does node get passed into this function?
+
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        if not self.head and self.tail:
+            return None
+
+        max_value = self.head.value
+        # reference to our current node as we traverse the list
+        current = self.head.next
+        # check to see if we're still at a valid list node
+        while current:
+            # check to see if the current value is greater than the max_value
+            if current.value > max_value:
+                # if so, update our max_value variable
+                max_value = current.value
+            # update the current node to the next node in the list
+            current = current.next
+        return max_value
