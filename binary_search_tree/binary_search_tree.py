d719c17e0c86da5b84bf253fa861e693b3ce57fb
damonbogich <damonbogich@gmail.com>
2020-06-11 19:34:55 -0400

    in_order_print working

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -66,7 +66,6 @@ class BSTNode:
         fn(self.value)
         if self.right is None and self.left is None:
             return None
-
         if self.right is not None and self.left is None:
             self.right.for_each(fn)
         if self.left is not None and self.right is None:
@@ -82,16 +81,75 @@ class BSTNode:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        #lowest number is always furthest to left
+        #base case?
+        if node is None:
+            return None
+        if node.left is None:
+            print(node.value)
+            self.in_order_print(node.right)
+        if node.left is not None:
+            #check if node.left has a value to its left
+            self.in_order_print(node.left)
+            print(node.value)
+            self.in_order_print(node.right)
+        
+            
+
+        #recursive case? (gets us closer to base case)
+        # self.in_order_print(self.left)
+        #your call stack is your clue
+
+
+
+
+
+
+
+
+
+
+    
+#     class Queue():
+#     #Storage?
+#     def __init__(self, size=None, storage=None):
+#         self.size = 0
+#         self.storage = LinkedList()
+#     #Find length of linkedlist
+#     def __len__(self):
+#         current = self.storage.head
+#         length = 0
+#         while current:
+#             length = length + 1
+#             current = current.next_node
+#         return length
+#     #add to the end of linked list (tail)
+#     def enqueue(self, value):
+#         return self.storage.add_to_tail(value)
+#     #Take away first item from queue
+#     def dequeue(self):
+#         return self.storage.remove_head()
+# #need to instantiate queue????
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
+        #use a queue
+        #start queue with root node
+
+        #while loop that checks size of queue
+            #pointer variable
+            #that updates at the beginning of each loop
         pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
+        #stack
+        #start stack with root node
+
+        #use while loop that checks stack size
+            #use pointer
         pass
 
     # Stretch Goals -------------------------
